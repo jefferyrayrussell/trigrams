d658b1ef572dd61382408a086b258d2bf2749483
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-10 13:53:11 -0700

    created initial files

---
+++ src/trigrams.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Trigrams can be used to mutate text into new, surreal, forms."""
+
+
+def main(path, num_word):
+    """Main function."""
+    pass
+
+if __name__ == '__main__':
+    pass
