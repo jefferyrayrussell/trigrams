26b5f4b603eb09968aa713eaf47675a87025b4f5
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-10 14:45:03 -0700

    created sanitize function to remove punction, double space, and new line character

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
 """Trigrams can be used to mutate text into new, surreal, forms."""
+import io
+
+import string
 
 
 def main(path, num_word):
     """Main function."""
     pass
+
+
+def sanitize(input):
+        """ Removes punctuation, double spaces, and new line character"""
+    punc = string.punctuation
+    for p in punc:
+        input = input.replace(p, " ")
+    input = input.replace("\n", " ")
+    input = input.replace("  ", " ")
+    return input
+
+
+def process_trigram(input):
+    """Takes input from text file and turns it into a dictionary."""
+
+def gener
+
 
 if __name__ == '__main__':
     pass
