fdff91582974ed4be390f53d247fc93b113cceec
Steven Than <steventhan11@gmail.com>
2016-08-10 21:19:38 -0700

    done test for sanitize, still need to break up long string

--- src/trigrams.py
+++ src/trigrams.py
@@ -8,12 +8,13 @@ def main(path, num_word):
     """Main function."""
     f = io.open('sherlock_small.txt', encoding='utf-8')
     content = f.read()
+    f.close()
     sanitized = sanitize(content)
     trigrams_dict = process_trigrams(sannitized)
 
 
 def sanitize(input):
-        """ Removes punctuation, double spaces, and new line character"""
+    """ Removes punctuation, double spaces, and new line character"""
     punc = string.punctuation
     for p in punc:
         input = input.replace(p, " ")
@@ -26,9 +27,6 @@ def process_trigrams(input):
     """Takes input from text file and turns it into a dictionary."""
 
     return trigrams_dict
-
-def gener
-
 
 if __name__ == '__main__':
     pass
